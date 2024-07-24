b88d303987d2c4c650b3863933788511ed6f7637
Ksenyabelll <kseniablokhina99@gmail.com>
2024-07-24 23:52:58 +0300

    md

--- Test_web.py
+++ Test_web.py
@@ -1,5 +1,19 @@
 
+class MyClass:
+    '''
+    description
+    '''
+    color = 'red'
+    ch = 2
 
-a = int(123)
+    def __str__(self):
+        return f'qwe {self.color}'
+a = MyClass()
 
+print(MyClass.ch)
 print(a)
+a.ch = 5
+print(MyClass.ch)
+print(a.ch)
+print(isinstance(a, MyClass))
+
